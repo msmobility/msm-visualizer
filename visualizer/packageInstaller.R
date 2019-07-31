4862848741b3a408d6b95392c58a710adef8b6a2
cllorca1 <carlos.llorca@tum.de>
2019-07-31 15:35:48 +0200

    add a script to install packages the first time

---
+++ visualizer/packageInstaller.R
@@ -0,0 +1,16 @@
+install.packages("readr") #for reading in data
+install.packages("readxl") #for reading excel files
+install.packages("sf") #for handling spatial data
+install.packages("zoo") #for manipulating data frames
+install.packages("plyr") #for recoding datag by mapping old- and new-name pairs
+install.packages("dplyr") #for manipulating data
+install.packages("stringr") #for manipulating strings
+install.packages("tidyr") #for tidying data
+install.packages("DT") #for producing interactive tables
+install.packages("plotly") #for producing interactive plots
+install.packages("tmap") #for producing maps
+install.packages("leaflet") #for producing interactive maps
+install.packages("viridis") #for picking colors
+install.packages("RColorBrewer") #for picking colors
+install.packages("stringr")
+install.packages("flexdashboard")
