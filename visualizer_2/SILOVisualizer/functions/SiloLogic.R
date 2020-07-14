4faa9fb707da68e95eb5e41ce1c6e2006e81d4c3
Rafael <rafael.nieto@tum.de>
2020-07-14 14:14:24 +0200

    Read data function for maps

--- visualizer_2/SILOVisualizer/functions/SiloLogic.R
+++ visualizer_2/SILOVisualizer/functions/SiloLogic.R
@@ -3,33 +3,60 @@
 ## Spatial function for routing options
 
 ## Function prepareSiloMap : Call the geographical database and filter according to the parameters in UI
-prepareSiloMap <- function (data, thisYear, zone_level, attribute){
+prepareSiloMap <- function (data, thisYear, zone_level, attribute, aggregationType){
   ## Conditional to choose between aggregate or keep dissagregate
   if(zone_level == FALSE){
 
     joinTable <- filter(data ,year == thisYear)
     spatialTable <- left_join(zones, joinTable, by="shp_id")
     
-
-
-    groupedTable <- spatialTable %>%
-    group_by(shp_muni)%>% summarize_at(vars(attribute), list(attribute = sum)) 
-    
-    names(groupedTable)[names(groupedTable) == "attribute"] <- attribute
-
-
-    print(thisYear)
-    print(zone_level)
-    print(attribute)
-    print(groupedTable)
-    
-    #operator 
+    if(aggregationType == "average") {
+      groupedTable <- spatialTable %>%
+      group_by(shp_muni)%>% summarize_at(vars(attribute), list(attribute = mean))
+      
+    } else {
+      groupedTable <- spatialTable %>%
+      group_by(shp_muni)%>% summarize_at(vars(attribute, shp_area), list(sum)) 
+      groupedTable <-groupedTable%>% mutate(!!attribute := ((groupedTable[[attribute]] / shp_area) * 1000000 ))
+    }
 
   } else {
-    joinTable <- filter(spatialData,year == 2020)
+    joinTable <- filter(data,year == thisYear)
     groupedTable <- left_join(zones, joinTable, by="shp_id")
+    if(aggregationType == "density"){
+      groupedTable <-groupedTable%>% mutate(!!attribute := ((groupedTable[[attribute]] / shp_area) * 1000000 ))
+    }
     
   }
   return(groupedTable)
   
-} 
+}
+prepareSiloMapScenario<-function(data, data2, thisYear, zone_level, attribute, aggregationType){
+  if(zone_level == FALSE){
+    joinTable <- filter(data ,year == thisYear)
+    joinTable2 <-filter(data2, year == thisYear)
+    joinScenarios <-left_join(joinTable, joinTable2, by="shp_id")
+    
+  }else{
+    ## Logic without grouping
+  }
+}
+
+prepareSiloMapLabels <- function (dataLabel, model, feature){
+  if (feature == "dwellings"){
+    
+  }else if (feature == "income"){
+    
+  }else if (feature == "accessibilities"){
+    
+  }else{
+  filteredLabels <- filter(dataLabel, myDataType == model & myItem == feature)
+  title <- filteredLabels[1,4]
+  legend <-filteredLabels[1,5]
+  }
+  return (c(title,legend))
+  return(legend)
+}
+dummycall <-function(int){
+  return(int)
+}
