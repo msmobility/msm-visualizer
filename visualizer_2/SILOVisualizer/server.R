dbc01a6a4faecd297706d67046252d31336eb3e0
Rafael Munoz <rlmunozn@unal.edu.co>
2020-08-19 11:16:59 +0200

    Delete old versions

--- visualizer_2/SILOVisualizer/server.R
+++ visualizer_2/SILOVisualizer/server.R
@@ -50,33 +50,33 @@ shinyServer(function(input, output) {
     output$hist <- renderPlot({hist(data())})
     output$value <-renderPrint({summary(data())})
     output$table <-renderDataTable({householdsTable()})
-
-## File loader
+    
     observeEvent(global$datapath, {loadfiles()})
-    
-    loadfiles <- reactive({
-    if (global$datapath == getwd()){}
-    else {
-      print(global$datapath)
-    
-    ## Define all tables
-      global$o_hhsize <- read.csv(paste(global$datapath,"aveHhSize.csv",sep="/", collapse = NULL))
-      global$o_c_owne <- read.csv(paste(global$datapath,"carOwnership.csv",sep="/", collapse = NULL))
-      global$o_com_di <- read.csv(paste(global$datapath,"commutingDistance.csv",sep="/", collapse = NULL))
-      global$o_dwelli <- read.csv(paste(global$datapath,"dwellings.csv",sep="/", collapse = NULL))
-      global$o_eventc <- read.csv(paste(global$datapath,"eventCounts.csv",sep="/", collapse = NULL))
-      global$o_hhAvIn <- read.csv(paste(global$datapath,"hhAveIncome.csv",sep="/", collapse = NULL))
-      global$o_hhSatR <- read.csv(paste(global$datapath,"hhSatisfactionByRegion.csv",sep="/", collapse = NULL))
-      global$o_hhSize <- read.csv(paste(global$datapath,"hhSize.csv",sep="/", collapse = NULL))
-      global$o_hhType <- read.csv(paste(global$datapath,"hhType.csv",sep="/", collapse = NULL))
-      global$o_laPaRa <- read.csv(paste(global$datapath,"labourParticipationRate.csv",sep="/", collapse = NULL))
-      global$o_lanReg <- read.csv(paste(global$datapath,"landRegions.csv",sep="/", collapse = NULL))
-      global$o_popYea <- read.csv(paste(global$datapath,"popYear.csv",sep="/", collapse = NULL))
-      global$o_spatialData <-read.csv(paste(global$datapath,"resultFileSpatial.csv",sep="/", collapse= NULL))
-      global$o_spatialData <-rename(global$o_spatialData, "shp_id" = "zone")
-            }
-    })
 
+    
+    
+## File loader
+    loadfiles <- reactive({
+        if (global$datapath == getwd()){}
+        else {
+            print(global$datapath)
+            
+            ## Define all tables
+            o_hhsize <- read.csv(paste(global$datapath,"aveHhSize.csv",sep="/", collapse = NULL))
+            o_c_owne <- read.csv(paste(global$datapath,"carOwnership.csv",sep="/", collapse = NULL))
+            o_com_di <- read.csv(paste(global$datapath,"commutingDistance.csv",sep="/", collapse = NULL))
+            o_dwelli <- read.csv(paste(global$datapath,"dwellings.csv",sep="/", collapse = NULL))
+            o_eventc <- read.csv(paste(global$datapath,"eventCounts.csv",sep="/", collapse = NULL))
+            o_hhAvIn <- read.csv(paste(global$datapath,"hhAveIncome.csv",sep="/", collapse = NULL))
+            o_hhSatR <- read.csv(paste(global$datapath,"hhSatisfactionByRegion.csv",sep="/", collapse = NULL))
+            o_hhSize <- read.csv(paste(global$datapath,"hhSize.csv",sep="/", collapse = NULL))
+            o_hhType <- read.csv(paste(global$datapath,"hhType.csv",sep="/", collapse = NULL))
+            o_laPaRa <- read.csv(paste(global$datapath,"labourParticipationRate.csv",sep="/", collapse = NULL))
+            o_lanReg <- read.csv(paste(global$datapath,"landRegions.csv",sep="/", collapse = NULL))
+            o_popYea <- read.csv(paste(global$datapath,"popYear.csv",sep="/", collapse = NULL))
+        }
+    })
+## Scenario file loader
 ## Scenario File loader
     observeEvent(global$datapath2, {loadfiles2()})
     
@@ -86,222 +86,127 @@ shinyServer(function(input, output) {
         print(global$datapath2)
         
         ## Define all tables
-      global$c_hhsize <- read.csv(paste(global$datapath2,"aveHhSize.csv",sep="/", collapse = NULL))
-      global$c_c_owne <- read.csv(paste(global$datapath2,"carOwnership.csv",sep="/", collapse = NULL))
-      global$c_com_di <- read.csv(paste(global$datapath2,"commutingDistance.csv",sep="/", collapse = NULL))
-      global$c_dwelli <- read.csv(paste(global$datapath2,"dwellings.csv",sep="/", collapse = NULL))
-      global$c_eventc <- read.csv(paste(global$datapath2,"eventCounts.csv",sep="/", collapse = NULL))
-      global$c_hhAvIn <- read.csv(paste(global$datapath2,"hhAveIncome.csv",sep="/", collapse = NULL))
-      global$c_hhSatR <- read.csv(paste(global$datapath2,"hhSatisfactionByRegion.csv",sep="/", collapse = NULL))
-      global$c_hhSize <- read.csv(paste(global$datapath2,"hhSize.csv",sep="/", collapse = NULL))
-      global$c_hhType <- read.csv(paste(global$datapath2,"hhType.csv",sep="/", collapse = NULL))
-      global$c_laPaRa <- read.csv(paste(global$datapath2,"labourParticipationRate.csv",sep="/", collapse = NULL))
-      global$c_lanReg <- read.csv(paste(global$datapath2,"landRegions.csv",sep="/", collapse = NULL))
-      global$c_popYea <- read.csv(paste(global$datapath2,"popYear.csv",sep="/", collapse = NULL))
-      global$c_spatialData <-read.csv(paste(global$datapath2,"resultFileSpatial.csv",sep="/", collapse= NULL))
-      global$c_spatialData <-rename(global$c_spatialData, "shp_id" = "zone")
+        c_hhsize <- read.csv(paste(global$datapath2,"aveHhSize.csv",sep="/", collapse = NULL))
+        c_c_owne <- read.csv(paste(global$datapath2,"carOwnership.csv",sep="/", collapse = NULL))
+        c_com_di <- read.csv(paste(global$datapath2,"commutingDistance.csv",sep="/", collapse = NULL))
+        c_dwelli <- read.csv(paste(global$datapath2,"dwellings.csv",sep="/", collapse = NULL))
+        c_eventc <- read.csv(paste(global$datapath2,"eventCounts.csv",sep="/", collapse = NULL))
+        c_hhAvIn <- read.csv(paste(global$datapath2,"hhAveIncome.csv",sep="/", collapse = NULL))
+        c_hhSatR <- read.csv(paste(global$datapath2,"hhSatisfactionByRegion.csv",sep="/", collapse = NULL))
+        c_hhSize <- read.csv(paste(global$datapath2,"hhSize.csv",sep="/", collapse = NULL))
+        c_hhType <- read.csv(paste(global$datapath2,"hhType.csv",sep="/", collapse = NULL))
+        c_laPaRa <- read.csv(paste(global$datapath2,"labourParticipationRate.csv",sep="/", collapse = NULL))
+        c_lanReg <- read.csv(paste(global$datapath2,"landRegions.csv",sep="/", collapse = NULL))
+        c_popYea <- read.csv(paste(global$datapath2,"popYear.csv",sep="/", collapse = NULL))
       }
     })
 
-## Dummy function to trigger maps
-    dummyfunc <-eventReactive(input$update, {dummycall(1)})
+## Reactive to choose type of representation
+    observeEvent(input$renderType,{renderSelector()})
+    renderSelector <-reactive({
+      if(input$renderType =='aspatial'){
+        # Call plot function
+      }
+      else{
+        # Call map function
+        
+      }
+    })
+    data <-eventReactive(global$datapath2, {prepareSiloMap(spatialData, input$year, input$zone_level, input$spatialLevel)})
     
 ## Plot Map
     output$siloMap <- renderLeaflet({
-      n <-dummyfunc()
+      print(input$spatialLevel)
+      groupedTable <- data()
+      groupedTable <-prepareSiloMap(spatialData, input$year, input$zone_level, input$spatialLevel)
+      print(groupedTable)
+  
       
-      ## Map color selection
-      if(input$comparisonSelector ==1){
-        farbe <- "-RdBu"
-      } else if (input$comparisonSelector ==2){
+      style <- 'pretty'
+      categories <- 10
+      d <- FALSE
+      if(d == FALSE){
+        #attribute <- "households"
         farbe <- "-RdBu"
       } else {
-        if(input$scenarioSelector == 1){
-        } else {
+        #attribute <- "households"
+        farbe <- "-RdBu"
+      }
+      if (input$spatialLevel == "dwellings"){
+        msmMap(groupedTable, "attribute", farbe, 'Viviendas','Label',
+               input$siloMapStyle, input$siloMapCategories)
+      } else if (input$spatialLevel == "accessibilities"){
+        msmMap(groupedTable, attribute, farbe, "Viviendas","Label", 
+               input$siloMapStyle, input$siloMapCategories)
+      } else if (input$spatialLevel == "incomes"){
+        msmMap(groupedTable, attribute, farbe, "Viviendas","Label",
+               input$siloMapStyle, input$siloMapCategories)
+      } else {
+        msmMap(groupedTable, input$spatialLevel, farbe,input$spatialLevel,"Label",
+               input$siloMapStyle, input$siloMapCategories)
+        #tmap_leaflet(tm_shape(groupedTable) +
+        #               tm_polygons("households", palette = farbe, title ="Hello World",style = 'pretty', n = 10) +
+        #               tm_layout("Hii"))
+      }
+    })
+    
+    
+    
+
+    
+    ## Observer to render new panel
+    #observeEvent(input$comparison, {renderFileLoader2()})
+    
+    ##File loader2
+    #renderFileLoader2 <- reactive({
+    #  if (input$comparison == TRUE){
+    #    shinyDirButton("dir2","Select scenario directory", "Select directory")
+    #  } else{}
+    #})
+
+    ## Filter function example
+    filterSpatialTable <- function(chosenYear,data){
+      joinTable <- filter(data,year == chosenYear)
+      spatialTable <- left_join(zones, joinTable, by="shp_id")
+    }
+    
+
+    
+        
+    ## Table implementation example
+    householdsTable <-reactive({
+        if (global$datapath == getwd()){}
+        else {
+            renderDataTable(hhsize)
+            
         }
-        farbe <- "YlOrBr"
-      }
-      ## Execute maps
-      
-      print("Execute map calculation")
-      print(names(spatialData()[[1]])[2])
-      dataSubset <-spatialData()[[1]]
-      print(dataSubset)
-      legend <- spatialData()[[2]]
-      attribute <- spatialData()[[3]]
-      print(attribute)
-      print(legend[2])
-      msmMap(dataSubset,attribute, farbe,legend[1],legend[2],
-             input$siloMapStyle, input$siloMapCategories)
+    })
+    
+
+# 
+
+    
+    
+        
+    #dtest <- eventReactive(global$datapath, {renderText(getdir)}, ignoreNULL = TRUE)
+    #output$folder <- 'hello'
+    
+    #output$folder <- renderText({
+    #dtest()
+    ##})
+    
+    ###
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
     })
-  
-  
-  
-#################################
-  # Modularize spatial inputs
-  spatialData <-reactive({
-    n <-dummyfunc()
-    print("Module spatialData is running")
-    if(input$spatialLevel == 'dwellings'){
-      
-      attribute <- input$sDwelling
-      aggregationType <- "density"
-      
-    }else if(input$spatialLevel == 'income'){
-      attribute <- input$sIncome
-      aggregationType <- "density"
-      
-    }else if(input$spatialLevel == 'accessibilities'){
-      attribute <- input$sAcc
-      aggregationType <- "average"
-      
-    }else if(input$spatialLevel == 'avePrice'){
-      attribute <- input$spatialLevel
-      aggregationType <- "average"
-      
-    }else {
-      attribute = input$spatialLevel
-      aggregationType <- "density"
-      
-    }
-    ## Database logic choose scenario and comparison type
-    if(input$comparisonSelector ==1){
-
-      isComparison <-TRUE
-      legend <- prepareSiloMapLabels(myLabels, "siloSpatial", attribute, isComparison)
-      originalDataSet <-prepareSiloMap(global$o_spatialData, input$year, input$zone_level, attribute, aggregationType)
-      scenarioDataSet <-prepareSiloMap(global$c_spatialData, input$year, input$zone_level, attribute, aggregationType)
-      
-      groupedTable <- compareScenarios(originalDataSet, scenarioDataSet,attribute)  
-      
-    } else if (input$comparisonSelector ==2){
-      farbe <- "-RdBu"
-      isComparison <-TRUE
-      
-      legend <- prepareSiloMapLabels(myLabels, "siloSpatial", attribute, isComparison)
-      originalDataSet <-prepareSiloMap(global$o_spatialData, initialYear, input$zone_level, attribute, aggregationType)
-      scenarioDataSet <-prepareSiloMap(global$o_spatialData, input$year, input$zone_level, attribute, aggregationType)
-      groupedTable <- compareScenarios(originalDataSet, scenarioDataSet,attribute)
-      
-    } else {
-      if(input$scenarioSelector == 1){
-        spatialDataSet <-global$o_spatialData
-      } else {
-        spatialDataSet <-global$c_spatialData
-      }
-
-      isComparison <-FALSE
-      legend <- prepareSiloMapLabels(myLabels, "siloSpatial", attribute, isComparison)
-      print("Entering groupedTable")
-      groupedTable <-prepareSiloMap(spatialDataSet, input$year, input$zone_level, attribute, aggregationType)
-      print("Groupedtable finished")
-
-    }
-    print("Module spatialData finished correctly")
-    print(groupedTable)
-    
-    return(list(groupedTable, legend, attribute))
-    
-  })
-#################################
-    
-#################################
-## Experimental map reactivity
-    geo <- eventReactive(input$map_shape_click, {
-      shinyjs::show("reactiveOutput6")
-      shinyjs::show("reactiveOutput5")
-      shinyjs::show("reactiveOutput4a1")
-      shinyjs::show("reactiveOutput4a")
-      shinyjs::show("reactiveOutput4")
-      shinyjs::show("reactiveOutput3")
-      shinyjs::show("reactiveOutput2a")
-      shinyjs::show("reactiveOutput2b")
-      shinyjs::show("reactiveOutput2c")
-      shinyjs::show("reactiveOutput1")
-      shinyjs::removeClass(class = "shinyjs-hide", selector = "hr")
-      shinyjs::removeClass(class = "shinyjs-hide", selector = ".kpi-group")
-      
-      click <- input$map_shape_click
-      print(click$id)
-      as.numeric(click$id)
-    })
-#################################
-  output$gisTable <- renderDataTable(
-    
-    select(as.data.frame(spatialData()[1]),-geometry)
-    )
-    
-  ##  select(as.data.frame(global$exportTable),-geometry)
-    
-  ## Downloader
-  output$downloadData <- downloadHandler(
-    filename = function() {
-      paste("export_example", ".csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(select(as.data.frame(spatialData()[1]),-geometry), file, row.names = TRUE)
-    }
-  )
-  output$downloadShape<-downloadHandler(
-    filename = function() { paste("shpExport","zip", sep = ".") },
-    content = function(file){
-      data <- spatialData()
-      temp_shp <- tempdir()
-      st_write(data, "temp_shp.shp")
-
-      zip_file <- file.path(temp_shp, "shapefile_shp.zip")
-      shp_files <- list.files(temp_shp,
-                              "temp_shp",
-                              full.names = TRUE)
-      zip_command <- paste("zip -j", 
-                           zip_file, 
-                           paste(shp_files, collapse = " "))
-      system(zip_command)
-      file.copy(zip_file, file)
-      file.remove(zip_file, shp_files)
-    }
-  )
-  #################################
-  ## Graphic logic
-  getAspatialData <- reactive({
-    n <-dummyfunc()
-    print("Running aspatial tables preparation")
-    if(input$aspatialLevel == 'overview'){
-      population_men <- aggregate(global$o_popYea$men, by=list(popYear= global$o_popYea$year), FUN=sum)
-      population_women <- aggregate(global$o_popYea$women, by=list(popYear= global$o_popYea$year), FUN=sum)
-      dwellings <- aggregate(global$o_dwelli$count, by=list(dwelling= global$o_dwelli$year), FUN=sum)
-      households <- aggregate(global$o_hhSize$count, by=list(households = global$o_hhSize$year), FUN = sum)
-      ## Join data
-      men <- population_men['x']
-      women <- population_women['x']
-      households <- households['x']
-      dwellings <- dwellings['x']
-      population <- data.frame(population_men['popYear'],population_men['x'],population_women['x'],households['x'],dwellings['x'])
-      population <- population%>%rename(men = x, women = x.1, households = x.2, dwellings = x.3 )
-      population$population <- population$men + population$women
-      return(population)
-    }else if (input$aspatialLevel == 'households'){
-      
-    }
-    
-
-  })
-  fig <- reactive({
-
-    fig <-plot_ly(getAspatialData(), x = ~popYear)
-    
-    fig <-fig%>%add_trace(y = ~population, name = 'Population', type = 'scatter', mode = 'lines')
-    fig <-fig%>%add_trace(y = ~men, name = 'Men',  type = 'scatter', mode ='lines') 
-    fig <-fig%>%add_trace(y = ~women, name = 'Women',  type = 'scatter', mode ='lines')
-    fig <-fig%>%add_trace(y = ~households, name = 'Households',  type = 'scatter', mode ='lines') 
-    fig <-fig%>%add_trace(y = ~dwellings, name = 'Dwellings',  type = 'scatter', mode ='lines')
-    fig<- fig%>%layout(title= "Overview")
-    return(fig)
-  })
-  output$siloPlot <-renderPlotly(
-    fig()
-  )
     
 
 })
