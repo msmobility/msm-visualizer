6bf63d68abc9187f5f675e326d1983af2b07df1f
Rafael <rafael.nieto@tum.de>
2020-11-03 10:36:47 +0100

    create generateLabels function

--- visualizer_2/SILOVisualizer/functions/SiloFunctions.R
+++ visualizer_2/SILOVisualizer/functions/SiloFunctions.R
@@ -20,3 +20,54 @@ labelSiloAspatial <- function(plot, attribute, change, switch){
                             xanchor = "left", yanchor = "bottom", legendtitle = TRUE, showarrow = F)) %>%
     layout(legend = list(y = 0.9, yanchor = "top"))
 }
+generateLabels <- function(fig, visualizationType, input) {
+  print("We are in the function")
+  print(menuSettings)
+  print("Hehehe")
+  print(input$aspatialLevel)
+  ## Filter labels
+  filteredLabels = filter(myLabels,myDataType == visualizationType)
+  if(input$aspatialLevel == 'overview'){
+    filteredLabels = filter(filteredLabels, myItem == input$aspatialLevel)
+  }else if(input$aspatialLevel == 'households'){
+    filteredLabels = filter(filteredLabels, myItem == input$HHLevel)
+  }else if(input$aspatialLevel == 'persons'){
+    print("PErsons levels, ")
+    print(input$personsLevel)
+    filteredLabels = filter(filteredLabels, myItem == input$personsLevel)
+  }else if(input$aspatialLevel == 'dwellings'){
+    filteredLabels = filter(filteredLabels, myItem == input$dwellingsLevel)
+  }else if(input$aspatialLevel == 'regional'){
+    filteredLabels = filter(filteredLabels, myItem == input$regionalLevel)
+  }else if(input$aspatialLevel == 'events'){
+    filteredLabels = filter(filteredLabels, myItem == input$eventsLevel)
+  }
+  if(input$switchView == FALSE){
+    switch <-1
+    filteredLabels = filter(filteredLabels, mySwitch == switch)
+  }else{
+    switch <-2
+    filteredLabels = filter(filteredLabels, mySwitch == switch)
+  }
+  
+  fig <-fig %>%layout(
+    title =filteredLabels$myTitle,
+    xaxis =list(title = filteredLabels$myX),
+    yaxis =list(title = filteredLabels$myY),
+    annotations = list(yref = 'paper',xref = 'paper', y = 0.95, x = 1.05, text = filteredLabels$myLegend)
+    #legend =list(title = "filteredLabels$myLegend")#,
+    #sliders =list(title = filteredLabels$myFrame)
+  )
+  ## Second filter level
+  #if(inp)
+  
+  
+  #print(input)
+  #print(input$aspatialLevel)
+
+  print("Filtered options")
+  print(filteredLabels)
+  ##session$userData$menuSettings <- filter(session$userData$menuSettings, required_file_1 != fileList[j] | is.na(required_file_1))
+  
+  return(fig)
+}
